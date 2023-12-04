5b1af4f8543b9c3196f1e0fef282e759fa828b25
Antoan Metodiev <Boksnem@abv.bg>
2023-12-04 21:09:02 +0200

    add task

---
+++ Stacks and Queues/Stacks and Queues - Lab/BrowserHistory.java
@@ -0,0 +1,28 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputs = scanner.nextLine();
+
+        ArrayDeque<String> stack = new ArrayDeque<>();
+        while (!inputs.equals("Home")) {
+
+            if (inputs.equals("back")) {
+                if (stack.size() <= 1) {
+                    System.out.println("no previous URLs");
+                    inputs = scanner.nextLine();
+                    continue;
+                } else {
+                    String saver = stack.pop();
+                    System.out.println(stack.peek());
+                }
+            } else {
+                stack.push(inputs);
+                System.out.println(inputs);
+            }
+            inputs = scanner.nextLine();
+        }
+    }
+}
