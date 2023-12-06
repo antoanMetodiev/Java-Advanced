935feefd543cd91978c442b4119ee34f1c88390f
Antoan Metodiev <Boksnem@abv.bg>
2023-12-06 21:34:56 +0200

    add task

---
+++ Stacks and Queues/Stacks and Queues - Lab/HotPotato.java
@@ -0,0 +1,29 @@
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class HotPotato {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] names = scanner.nextLine().split(" ");
+        ArrayDeque<String> kids = new ArrayDeque<>();
+
+        Collections.addAll(kids, names);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int counter = 0;
+        while (kids.size() > 1) {
+
+            if (counter == n) {
+                System.out.println("Removed " + kids.pollLast());
+                counter = 0;
+            } else {      //   Sam John Sara
+                counter++;
+                String currentName = kids.poll();
+                kids.offerLast(currentName);
+            }
+        }
+        System.out.println("Last is " + kids.poll());
+    }
+}
