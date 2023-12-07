991fb0e58b7c6f8f4e7bac6c9954df5abc6feec7
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2023-12-07 20:07:40 +0200

    Update BasicStackOperations.java

--- Stacks and Queues/Stacks and Queues - Exercise/BasicStackOperations.java
+++ Stacks and Queues/Stacks and Queues - Exercise/BasicStackOperations.java
@@ -8,22 +8,18 @@ public class BasicStackOperations {
         Scanner scanner = new Scanner(System.in);
 
         String[] inputData = scanner.nextLine().split(" ");
-
-        int n = Integer.parseInt(inputData[0]);  // Kойто трябва да бъдат добавени в стека
-        int s = Integer.parseInt(inputData[1]);  // Който трябва да бъдат изтрити в стека
-        int x = Integer.parseInt(inputData[2]);  // Този елемент трябва да се провери дали присъства
-                                                       // и да се изпише true или false
+        int n = Integer.parseInt(inputData[0]);  
+        int s = Integer.parseInt(inputData[1]); 
+        int x = Integer.parseInt(inputData[2]); 
 
         String[] inputNumbers = scanner.nextLine().split("\\s+");
 
-        // Тук се добавят в стека
         Deque<Integer> numbersStack = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
             int el = Integer.parseInt(inputNumbers[i]);
             numbersStack.push(el);
         }
 
-        // Тук изтриваме елементите
         while (s > 0) {
             numbersStack.pop();
             s--;
@@ -33,7 +29,6 @@ public class BasicStackOperations {
             if (numbersStack.contains(x)) {
                 System.out.println("true");
             } else {
-                // Намираме най-малкия елемент в stack-а
                 System.out.println(Collections.min(numbersStack));
             }
         } else {
