6921e5944f34a26f00a896e9f0638bdff92cb6b7
Antoan Metodiev <Boksnem@abv.bg>
2023-12-08 21:29:19 +0200

    add task

---
+++ Stacks and Queues/Stacks and Queues - Exercise/BalancedParentheses.java
@@ -0,0 +1,66 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class BalancedParentheses {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = (scanner.nextLine().split(""));
+
+        Deque<String> openBracets = new ArrayDeque<>();
+
+        for (int i = 0; i < input.length; i++) {
+
+            String currentBracet = input[i];  // Скобата която проверяваме в момента
+
+            switch (currentBracet) {
+                case ")":
+
+                    if (openBracets.isEmpty()) {
+                        System.out.println("NO");
+                        return;
+                    }
+
+                    if (!openBracets.pop().equals("(")) {
+                        System.out.println("NO");
+                        return;
+                    }
+
+                    break;
+                case "]":
+
+                    if (openBracets.isEmpty()) {
+                        System.out.println("NO");
+                        return;
+                    }
+
+                    if (!openBracets.pop().equals("[")) {
+                        System.out.println("NO");
+                        return;
+                    }
+
+                    break;
+                case "}":
+
+                    if (openBracets.isEmpty()) {
+                        System.out.println("NO");
+                        return;
+                    }
+
+                    if (!openBracets.pop().equals("{")) {
+                        System.out.println("NO");
+                        return;
+                    }
+
+                    break;
+                default:
+                    openBracets.push(currentBracet);
+                    break;
+            }
+        }
+
+        System.out.println("YES");
+    }
+}
