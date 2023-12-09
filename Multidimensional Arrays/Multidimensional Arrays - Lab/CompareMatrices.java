106ae7ec5929fad6d8178518f4458a163d313925
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2023-12-09 20:54:30 +0200

    Update CompareMatrices.java

--- Multidimensional Arrays/Multidimensional Arrays - Lab/CompareMatrices.java
+++ Multidimensional Arrays/Multidimensional Arrays - Lab/CompareMatrices.java
@@ -4,38 +4,31 @@ import java.util.Scanner;
 public class CompareMatrices {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int[] rowsAndColumns = Arrays.stream(scanner.nextLine()
                         .split("\\s+"))
                 .mapToInt(Integer::parseInt).toArray();
 
-        int rows = rowsAndColumns[0];  // редове
-        int columns = rowsAndColumns[1];  // колони
+        int rows = rowsAndColumns[0];  
+        int columns = rowsAndColumns[1];  
 
         int rowsSaver = rows;
         int columnsSaver = columns;
 
-
-        // Пълним първата матрица:
         int[][] firstMatrix = new int[rows][columns];
         for (int i = 0; i < rows; i++) {
 
             String[] columnsNumbers = scanner.nextLine().split("\\s+");
             for (int j = 0; j < columnsNumbers.length; j++) {
-
                 firstMatrix[i][j] = Integer.parseInt(columnsNumbers[j]);
-
             }
         }
-
-        // Пълним втората матрица:
-
+        
         rowsAndColumns = Arrays.stream(scanner.nextLine()
                         .split("\\s+"))
                 .mapToInt(Integer::parseInt).toArray();
 
-        rows = rowsAndColumns[0];  // редове
-        columns = rowsAndColumns[1];  // колони
+        rows = rowsAndColumns[0]; 
+        columns = rowsAndColumns[1];  
 
         if (rowsSaver != rows) {
             System.out.println("not equal");
@@ -46,18 +39,15 @@ public class CompareMatrices {
             return;
         }
 
-        // secondMatrix===>>:
         int[][] secondMatrix = new int[rows][columns];
         for (int i = 0; i < rows; i++) {
 
             String[] columnsNumbers = scanner.nextLine().split("\\s+");
             for (int j = 0; j < columns; j++) {
-
                 secondMatrix[i][j] = Integer.parseInt(columnsNumbers[j]);
             }
         }
 
-        // checking if they are equals:
         if (equalsChecking(firstMatrix, secondMatrix)) {
             System.out.println("equal");
         } else {
@@ -66,23 +56,19 @@ public class CompareMatrices {
     }
 
     private static boolean equalsChecking(int[][] firstMatrix, int[][] secondMatrix) {
-
         int rows = firstMatrix.length;
         int columns = firstMatrix[0].length;
-
+        
         for (int i = 0; i < rows; i++) {
-
             for (int j = 0; j < columns; j++) {
 
                 int currentFirstMatrixEl = firstMatrix[i][j];
                 int currentSecondMatrixEl = secondMatrix[i][j];
-
                 if (currentFirstMatrixEl != currentSecondMatrixEl) {
                     return false;
                 }
             }
         }
-        // Ако издържи всичките ни проверки си ни дава "true"
         return true;
     }
 }
