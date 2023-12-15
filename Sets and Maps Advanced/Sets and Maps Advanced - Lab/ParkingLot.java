7c1782d1c262b96b222ae510f934f474844e1c11
Antoan Metodiev <Boksnem@abv.bg>
2023-12-15 20:32:04 +0200

    add task

---
+++ Sets and Maps Advanced/Sets and Maps Advanced - Lab/ParkingLot.java
@@ -0,0 +1,40 @@
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Set<String> carNumbers = new LinkedHashSet<>();
+        String line = scanner.nextLine();
+
+        while (!"END".equals(line)) {
+
+
+            String[] tokens = line.split(",\\s+");
+            String action = tokens[0];
+
+            String number = tokens[1];
+
+            switch (action) {
+                case "IN":
+                    carNumbers.add(number);
+                    break;
+                case "OUT":
+                    carNumbers.remove(number);
+                    break;
+            }
+
+            line = scanner.nextLine();
+        }
+
+        if (carNumbers.isEmpty()) {
+            System.out.println("Parking Lot is Empty");
+
+        }
+        else {
+            carNumbers.forEach(System.out::println);
+        }
+    }
+}
