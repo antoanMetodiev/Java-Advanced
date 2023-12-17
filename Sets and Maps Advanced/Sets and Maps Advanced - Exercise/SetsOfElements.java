40cb2fc6a5d51cb9fa150ac1c43e8e6bd9bcced2
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2023-12-17 20:02:21 +0200

    Update SetsOfElements.java

--- Sets and Maps Advanced/Sets and Maps Advanced - Exercise/SetsOfElements.java
+++ Sets and Maps Advanced/Sets and Maps Advanced - Exercise/SetsOfElements.java
@@ -6,22 +6,19 @@ import java.util.Set;
 public class SetsOfElements {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         String[] input = scanner.nextLine().split("\\s+");
         int n = Integer.parseInt(input[0]);
         int m = Integer.parseInt(input[1]);
 
         Set<String> firstGroup = new LinkedHashSet<>();
         Set<String> secondGroup = new LinkedHashSet<>();
-
         for (int i = 0; i < n; i++) {
             firstGroup.add(scanner.nextLine());
         }
         for (int i = 0; i < m; i++) {
             secondGroup.add(scanner.nextLine());
         }
-        firstGroup.retainAll(secondGroup);  // Запазва всички елементи който присъстват и в 2та Set-a
-
+        firstGroup.retainAll(secondGroup);  
         firstGroup.forEach(e -> System.out.print(e + " "));
     }
 }
