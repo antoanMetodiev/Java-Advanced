3abeb24322be5669f1066e8c4cf522ad7836f5ec
Antoan Metodiev <Boksnem@abv.bg>
2023-12-17 20:03:08 +0200

    add task

---
+++ Sets and Maps Advanced/Sets and Maps Advanced - Exercise/PeriodicTable.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class PeriodicTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        Set<String> compounds = new TreeSet<>();
+        for (int i = 0; i < n; i++) {
+
+            Arrays.stream(scanner.nextLine()
+                            .split("\\s+"))
+                    .forEach(e -> compounds.add(e));
+        }
+        compounds.forEach(e -> System.out.print(e + " "));
+    }
+}
