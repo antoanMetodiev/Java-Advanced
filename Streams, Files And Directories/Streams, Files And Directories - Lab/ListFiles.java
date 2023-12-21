5088f75aded25c7eaee7862c11faef59de863b09
Antoan Metodiev <Boksnem@abv.bg>
2023-12-21 20:40:29 +0200

    add task

---
+++ Streams, Files And Directories/Streams, Files And Directories - Lab/ListFiles.java
@@ -0,0 +1,21 @@
+import java.io.File;
+
+public class ListFiles {
+    public static void main(String[] args) {
+
+
+        File file = new File("D:\\Programming\\Java\\Courses\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\Files-and-Streams");
+
+        if (file.exists()) {
+            if (file.isDirectory()) {
+
+                File[] allFiles = file.listFiles();
+                for (File f : allFiles) {
+                    if (!f.isDirectory()) {
+                        System.out.printf("%s: [%s]\n", f.getName(), f.length());
+                    }
+                }
+            }
+        }
+    }
+}
