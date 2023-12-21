a96039ef2e82d7950f39dfd2565be87119bcb648
Antoan Metodiev <Boksnem@abv.bg>
2023-12-21 20:41:34 +0200

    add task

---
+++ Streams, Files And Directories/Streams, Files And Directories - Lab/SortLines.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+
+public class SortLines {
+    public static void main(String[] args) throws IOException {
+
+        Path inputPath = Paths.get("D:\\Programming\\Java\\Courses\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt");
+        Path outputPath = Paths.get("D:\\Programming\\Java\\Courses\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\06.SortLinesOutput.txt");
+
+        List<String> lines = Files.readAllLines(inputPath);
+        Collections.sort(lines);
+
+        Files.write(outputPath, lines);
+    }
+}
