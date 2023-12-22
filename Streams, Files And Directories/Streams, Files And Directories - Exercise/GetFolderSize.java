183710df035a6e98fec3ba0da85b6235fcd21f4a
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2023-12-22 20:24:14 +0200

    Update GetFolderSize.java

--- Streams, Files And Directories/Streams, Files And Directories - Exercise/GetFolderSize.java
+++ Streams, Files And Directories/Streams, Files And Directories - Exercise/GetFolderSize.java
@@ -2,12 +2,10 @@ import java.io.File;
 
 public class GetFolderSize {
     public static void main(String[] args) {
-
+        
         String path = "D:\\Programming\\Java\\Courses\\Java Advanced\\04.Streams, Files And Directories\\Streams, Files And Directories - Exercise\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\Exercises Resources";
 
         File folder = new File(path);
-
-        // Дебъгни задачата 1-път
         int sum = 0;
         if (folder.exists()) {
             if (folder.isDirectory()) {
