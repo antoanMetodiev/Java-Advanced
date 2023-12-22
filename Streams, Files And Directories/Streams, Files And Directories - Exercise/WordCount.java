70889fc4b23ac2ffd85cc3463b88bf55ac198e7a
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2023-12-22 20:25:19 +0200

    Update WordCount.java

--- Streams, Files And Directories/Streams, Files And Directories - Exercise/WordCount.java
+++ Streams, Files And Directories/Streams, Files And Directories - Exercise/WordCount.java
@@ -7,7 +7,6 @@ public class WordCount {
     public static void main(String[] args) throws IOException {
 
         Map<String, Integer> words = new LinkedHashMap<>();
-
         try (
                 Scanner firstReader = new Scanner(new FileReader("D:\\Programming\\Java\\Courses\\Java Advanced\\04.Streams, Files And Directories\\Streams, Files And Directories - Exercise\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\Exercises Resources\\words.txt"));
                 BufferedReader secondReader = new BufferedReader(new FileReader("D:\\Programming\\Java\\Courses\\Java Advanced\\04.Streams, Files And Directories\\Streams, Files And Directories - Exercise\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\Exercises Resources\\text.txt"))
@@ -19,20 +18,17 @@ public class WordCount {
 
             String[] elsInText = text.get(0).split(" ");
             for (String word : elsInText) {
-
                 if (words.containsKey(word)) {
                     int previousValue = words.get(word);
                     words.put(word, ++previousValue);
                 }
             }
-
             BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\Programming\\Java\\Courses\\Java Advanced\\04.Streams, Files And Directories\\Streams, Files And Directories - Exercise\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\Exercises Resources\\result.txt"));
 
             for (Map.Entry<String, Integer> word : words.entrySet()) {
                 writer.write(String.format("%s - %d\n", word.getKey(), word.getValue()));
             }
             writer.close();
-
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
