6075a3ba9e92dc73e256d6051557a010a822bfad
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2023-12-25 19:18:46 +0200

    Update CustomMinFunction.java

--- Functional Programming/Functional Programming - Exercise/CustomMinFunction.java
+++ Functional Programming/Functional Programming - Exercise/CustomMinFunction.java
@@ -7,18 +7,11 @@ import java.util.function.Function;
 public class CustomMinFunction {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-//        System.out.println(Arrays.stream(scanner.nextLine()
-//                .split("\\s+"))
-//                .mapToInt(Integer::parseInt).min().orElse(0));
-
-        // Да си прегледам кода внимателно :
         Integer[] numbers = Arrays.stream(scanner.nextLine()
                         .split("\\s+"))
                 .map(Integer::parseInt).toArray(Integer[]::new);
-        //------------------------------------------------------->
-
+        
         Function<Integer[], Integer> findMinNum = arr -> Collections.min(List.of(arr));
-
         System.out.println(findMinNum.apply(numbers));
     }
 }
