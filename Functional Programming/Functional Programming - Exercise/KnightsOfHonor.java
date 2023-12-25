1c339eddd156c3ee3263611c754760f5a63000bb
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2023-12-25 19:19:59 +0200

    Update KnightsOfHonor.java

--- Functional Programming/Functional Programming - Exercise/KnightsOfHonor.java
+++ Functional Programming/Functional Programming - Exercise/KnightsOfHonor.java
@@ -5,12 +5,8 @@ import java.util.function.Consumer;
 public class KnightsOfHonor {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         String[] inputNames = scanner.nextLine().split("\\s+");
-
         Consumer<String> printer = e -> System.out.println("Sir " + e);
-
         Arrays.stream(inputNames).forEach(printer);
-
     }
 }
