7f46bb5b74f7cba313ff23c55a7b2085c82bb6bd
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2023-12-26 20:33:37 +0200

    Update FindTheSmallestElement.java

--- Functional Programming/Functional Programming - Exercise/FindTheSmallestElement.java
+++ Functional Programming/Functional Programming - Exercise/FindTheSmallestElement.java
@@ -6,22 +6,16 @@ import java.util.stream.Collectors;
 public class FindTheSmallestElement {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         List<String> numbers = Arrays.stream(scanner.nextLine()
                         .split("\\s+"))
                 .collect(Collectors.toList());
 
         for (int i = 0; i < numbers.size(); i++) {
-
-//            if (numbers.isEmpty()) {
-//                break;
-//            }
             if (numbers.get(i).contains(".")) {
                 numbers.remove(numbers.get(i));
                 i--;
             }
         }
-
         if (!numbers.isEmpty()) {
             int minNum = numbers.stream().mapToInt(Integer::parseInt).min().getAsInt();
             System.out.println(numbers.lastIndexOf("" + minNum));
