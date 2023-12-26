3988e00022c1b07d6733ce156013a4fa7901181f
Antoan Metodiev <Boksnem@abv.bg>
2023-12-26 20:31:19 +0200

    add task

---
+++ Functional Programming/Functional Programming - Exercise/CustomComparator.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class CustomComparator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine()
+                        .split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        numbers.stream().filter(e -> e % 2 == 0).sorted()
+                .forEach(e -> System.out.print(e + " "));
+
+        numbers.stream().filter(e -> e % 2 != 0).sorted()
+                .forEach(e -> System.out.print(e + " "));
+    }
+}
