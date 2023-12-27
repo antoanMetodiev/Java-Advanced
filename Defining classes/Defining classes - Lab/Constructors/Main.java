80afc392cef7592b6518b88fd0eb478b481403f2
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2023-12-27 19:35:09 +0200

    Update Main.java

--- Defining classes/Defining classes - Lab/Constructors/Main.java
+++ Defining classes/Defining classes - Lab/Constructors/Main.java
@@ -6,15 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println(Car.sayHello());
         int n = Integer.parseInt(scanner.nextLine());
         for (int i = 0; i < n; i++) {
 
             String[] carData = scanner.nextLine().split("\\s+");
             String model = carData[0];
             Car car;
-
-            System.out.println(Car.sayHello());
 
             if (carData.length == 3) {
                 String type = carData[1];
