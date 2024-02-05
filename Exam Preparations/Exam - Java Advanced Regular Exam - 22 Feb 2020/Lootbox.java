cf1d43b45e9adb66091f8fa7484c503e6f6be1de
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-02-05 19:14:32 +0200

    Update Lootbox.java

--- Exam Preparations/Exam - Java Advanced Regular Exam - 22 Feb 2020/Lootbox.java
+++ Exam Preparations/Exam - Java Advanced Regular Exam - 22 Feb 2020/Lootbox.java
@@ -6,32 +6,26 @@ import java.util.Scanner;
 public class Lootbox {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         Deque<Integer> firstBox = new ArrayDeque<>();
         Arrays.stream(scanner.nextLine().split("\\s+")).forEach(e -> firstBox.offer(Integer.parseInt(e)));
-
 
         Deque<Integer> secondBox = new ArrayDeque<>();
         Arrays.stream(scanner.nextLine().split("\\s+")).forEach(e -> secondBox.push(Integer.parseInt(e)));
 
         int allItemSum = 0;
-
         while (!firstBox.isEmpty() && !secondBox.isEmpty()) {
 
             int sum = firstBox.peek() + secondBox.peek();
-
             if (sum % 2 == 0) {
-
                 allItemSum += sum;
                 firstBox.poll();
                 secondBox.pop();
             } else {
-
                 int removedEl = secondBox.pop();
                 firstBox.offer(removedEl);
             }
         }
-
+        // print Output:
         if (firstBox.isEmpty()) {
             System.out.println("First lootbox is empty");
         } else {
@@ -43,11 +37,6 @@ public class Lootbox {
         } else {
             System.out.print("Your loot was poor... ");
         }
-
         System.out.print("Value: " + allItemSum);
-
-
-
-
     }
 }
