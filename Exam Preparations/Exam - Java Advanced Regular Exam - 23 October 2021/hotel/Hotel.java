f4526a1aaf849321fb5502b555aba47daaf89a7e
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-02-07 14:09:31 +0200

    Update Hotel.java

--- Exam Preparations/Exam - Java Advanced Regular Exam - 23 October 2021/hotel/Hotel.java
+++ Exam Preparations/Exam - Java Advanced Regular Exam - 23 October 2021/hotel/Hotel.java
@@ -14,31 +14,25 @@ public class Hotel {
         this.roster = new ArrayList<>();
     }
 
-    // ⦁ Method add(Person person) - adds an entity to the roster if there is room for it
     public void add(Person person) {
         if (this.roster.size() < this.capacity) {
             this.roster.add(person);
         }
     }
 
-    //⦁	Method remove(String name) - removes a person by given name, if such exists, and returns boolean
     public boolean remove(String name) {
         return this.roster.removeIf(e -> e.getName().equals(name));
     }
 
-    //⦁	Method getPerson(String name, String hometown) – returns the people with the given name and hometown or
-    // null if there is no such person.
     public Person getPerson(String name, String hometown) {
         return this.roster.stream().filter(e -> e.getName().equals(name) && e.getHometown().equals(hometown))
                 .findFirst().orElse(null);
     }
 
-    //⦁	Getter getCount() – returns the number of people.
     public int getCount() {
         return this.roster.size();
     }
 
-    //⦁	getStatistics() – returns a String in the following format:
     public String getStatistics() {
         StringBuilder sb = new StringBuilder(String.format("The people in the hotel %s are:", this.name));
         this.roster.forEach(e-> {
