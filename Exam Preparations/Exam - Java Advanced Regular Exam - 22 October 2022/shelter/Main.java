9157bb0f62309b03080b1058394789136443d2b2
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-02-14 21:33:22 +0200

    Update Main.java

--- Exam Preparations/Exam - Java Advanced Regular Exam - 22 October 2022/shelter/Main.java
+++ Exam Preparations/Exam - Java Advanced Regular Exam - 22 October 2022/shelter/Main.java
@@ -2,42 +2,23 @@ package shelter;
 
 public class Main {
     public static void main(String[] args) {
-        // Initialize the repository
         Shelter shelter = new Shelter(15);
-// Initialize entity
         Animal animal = new Animal("Rex", 7, "Sara");
-
-// Print Animal
-        System.out.println(animal); // Rex 7 (Sara)
-
-// Add Animal
+        System.out.println(animal); 
         shelter.add(animal);
-
-// Remove Animal
-        System.out.println(shelter.remove("Rex")); // true
-        System.out.println(shelter.remove("Cayra")); // false
-
+        
+        System.out.println(shelter.remove("Rex"));
+        System.out.println(shelter.remove("Cayra")); 
         Animal animal1 = new Animal("Bela", 3, "Sia");
         Animal animal2 = new Animal("Stormy", 4, "George");
-
         shelter.add(animal1);
         shelter.add(animal2);
-
-// Get Oldest Animal
+        
         Animal oldestAnimal = shelter.getOldestAnimal();
-        System.out.println(oldestAnimal); // Stormy 4 (George)
-
-// Get Animal
+        System.out.println(oldestAnimal); 
         Animal animal3 = shelter.getAnimal("Bela", "Sia");
-        System.out.println(animal3); // Bela 3 (Sia)
-
-// Count
-        System.out.println(shelter.getCount()); // 2
-
-// Get Statistics
+        System.out.println(animal3); 
+        System.out.println(shelter.getCount()); 
         System.out.println(shelter.getStatistics());
-//The shelter has the following animals:
-//Bela Sia
-//Stormy George
     }
 }
