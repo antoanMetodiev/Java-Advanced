315cd37c8086ebe39cb93a2a5f322bc7fd38a043
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-02-16 19:21:04 +0200

    Update House.java

--- Exam Preparations/Exam - Java Advanced Regular Exam - 21 October 2023/halloween/House.java
+++ Exam Preparations/Exam - Java Advanced Regular Exam - 21 October 2023/halloween/House.java
@@ -12,33 +12,23 @@ public class House {
         this.capacity = capacity;
     }
 
-    // ⦁	Method addKid(Kid kid) – adds an entity to the data if there is an empty space for the kid.
-
     public void addKid(Kid kid) {
         if (this.capacity > this.data.size()) {
             this.data.add(kid);
         }
     }
 
-    //⦁	Method removeKid(String name) – removes the kid by given name, if such exists, and returns boolean.
-
     public boolean removeKid(String name) {
         return this.data.removeIf(e -> e.getName().equals(name));
     }
-
-    //⦁	Method getKid(String street) – returns the kid of a given street or null if no such kid exists.
 
     public Kid getKid(String street) {
         return this.data.stream().filter(e -> e.getStreet().equals(street)).findFirst().orElse(null);
     }
 
-    //⦁	Getter getAllKids() – returns the number of kids.
-
     public int getAllKids() {
         return this.data.size();
     }
-
-    //⦁	getStatistics() – returns a String in the following format:
 
     public String getStatistics() {
         StringBuilder sb = new StringBuilder("Children who visited a house for candy:");
@@ -48,11 +38,4 @@ public class House {
         });
         return sb.toString();
     }
-
-    //" Children who visited a house for candy:
-    //		 {name} from {street} street
-    //          {name} from {street} street
-    //          (…)"
-
-
 }
